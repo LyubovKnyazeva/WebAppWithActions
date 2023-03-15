9c14a471f4c7a1893ee4662ddecbba5c77760af2
Lyubov <lyubaknyaz@gmail.com>
2023-03-15 21:30:23 +0300

    added pr check piplane

--- WebApplication_15.03.2023_WithActions.Test.Unit/UnitTest1.cs
+++ WebApplication_15.03.2023_WithActions.Test.Unit/UnitTest1.cs
@@ -5,7 +5,9 @@ namespace WebApplication_15._03._2023_WithActions.Test.Unit
         [Fact]
         public void Test1()
         {
+            var actual = 10;
 
+            Assert.Equal(10, actual);
         }
     }
 }
